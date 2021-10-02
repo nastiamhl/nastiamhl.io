afafa09a6a8f44b13aa3dcdc929727a87da234b0
Anastasia Mihal <post.nastya@gmail.com>
2021-10-02 20:18:25 +0200

    webpack scss& html build refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
 
 module.exports = {
@@ -7,21 +8,43 @@ module.exports = {
         path: path.resolve(__dirname, 'docs')
     },
     module: {
-        rules: [{
-            test: /\.m?js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
                 }
-            }
-        }]
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader },
+                    { loader: "css-loader" },
+                    { loader: "sass-loader" }
+                ]
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+        ]
+    },
+    devServer: {
+        open: true,
+        liveReload: true,
+        watchFiles: ['src/**/*'],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            filename: 'index.html',
             template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
         })
     ],
 }
